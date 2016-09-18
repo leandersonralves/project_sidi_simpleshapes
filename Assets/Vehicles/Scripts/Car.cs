df9715b6c34be8c39ee3b2a2a3a79852a583256a
leanderson <leo_ribeiro_alves@yahoo.com.br>
2016-09-18 01:47:02 -0300

    Initial commit, added Player and Car scripts, a car 3D model.

---
+++ Assets/Vehicles/Scripts/Car.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum TurnSide
+{
+	Center,
+	Left,
+	Right
+}
+
+[RequireComponent(typeof(Rigidbody))]
+public abstract class Car : MonoBehaviour
+{
+	[SerializeField]
+	private float maxSpeed = 20f;
+	[SerializeField]
+	private float maxTorque = 20f;
+	[SerializeField]
+	private float brakeTorque = 150f;
+	[SerializeField]
+	private WheelCollider[] turnableWheels;
+	[SerializeField]
+	private float maxAngleWheel = 35f;
+	[SerializeField]
+	private WheelCollider[] wheelsWithTraction;
+
+	private Rigidbody m_rigidbody;
+
+	void Awake ()
+	{
+		m_rigidbody = GetComponent<Rigidbody> ();
+	}
+
+	protected void Acellerate (float valueNormalized)
+	{
+		float torque = Mathf.Clamp01 (valueNormalized) * maxTorque;
+		for (int i = 0; i < wheelsWithTraction.Length; i++) {
+			wheelsWithTraction [i].motorTorque = torque;
+		}
+
+		if(m_rigidbody.velocity.magnitude > maxSpeed)
+		{
+			m_rigidbody.velocity = m_rigidbody.velocity.normalized * maxSpeed;
+		}
+	}
+
+	protected void Brake ()
+	{
+		for (int i = 0; i < turnableWheels.Length; i++) {
+			turnableWheels [i].brakeTorque = brakeTorque;
+		}
+		for (int i = 0; i < wheelsWithTraction.Length; i++) {
+			wheelsWithTraction [i].brakeTorque = brakeTorque;
+		}
+	}
+
+	protected void Turn (TurnSide turnSide, float valueNormalized)
+	{
+		valueNormalized = Mathf.Clamp01(Mathf.Abs (valueNormalized));
+
+		float angle = 0f;
+		switch(turnSide)
+		{
+		case TurnSide.Left:
+			angle = maxAngleWheel * valueNormalized * -1f;
+			break;
+
+		case TurnSide.Right:
+			angle = maxAngleWheel * valueNormalized;
+			break;
+		}
+
+		valueNormalized = Mathf.Clamp01 (valueNormalized);
+		for (int i = 0; i < turnableWheels.Length; i++) {
+			turnableWheels [i].steerAngle = angle;
+		}
+	}
+}
---
+++ Assets/Vehicles/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Car {
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+		float accelerate = Input.GetAxis("Vertical");
+		if (!Mathf.Approximately (accelerate, 0f))
+			Acellerate (accelerate);
+
+		if (Input.GetKey (KeyCode.Space))
+			Brake ();
+
+		float turn = Input.GetAxis("Horizontal");
+
+		if (Mathf.Approximately (turn, 0))
+			Turn (TurnSide.Center, 0);
+		else {
+			if(turn > 0)
+				Turn (TurnSide.Right, turn);
+			else
+				Turn (TurnSide.Left, turn);
+		}
+	}
+}
