a62212a55f5326ca8f9bb313e43105efc9ad45d0
leo_ribeiro <leo_ribeiro>
2016-09-22 20:33:28 -0300

    Adicionado implementação inicial do Menus.

---
+++ Assets/Scripts/HUD/Background.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Background : MonoBehaviour
+{
+    public float timeFade;
+    public Color backgroundColor;
+
+    public delegate void OnCompleteFade();
+    public OnCompleteFade onCompleteFadeIn;
+    public OnCompleteFade onCompleteFadeOut;
+
+    Coroutine fade;
+    Image backgroundImage;
+
+    void Awake ()
+    {
+        backgroundImage = GetComponentInChildren<Image>();
+    }
+
+    public void Fade()
+    {
+        fade = StartCoroutine(Fade(true));
+    }
+    
+    IEnumerator Fade (bool isIn)
+    {
+        //Fade In
+        backgroundImage.CrossFadeAlpha(1f, timeFade, true);
+        float timeLeft = 0f;
+        while (timeLeft < timeFade)
+        {
+            timeLeft += Time.deltaTime;
+            yield return null;
+        }
+
+        if (onCompleteFadeIn != null)
+            onCompleteFadeIn.Invoke();
+        
+        //Fade Out
+        backgroundImage.CrossFadeAlpha(0f, timeFade, true);
+        timeLeft = 0f;
+        while (timeLeft < timeFade)
+        {
+            timeLeft += Time.deltaTime;
+            yield return null;
+        }
+
+        if (onCompleteFadeOut != null)
+            onCompleteFadeOut.Invoke();
+    }
+}
---
+++ Assets/Scripts/HUD/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    private string nextSceneLoad = string.Empty;
+    Background background;
+
+    void Awake()
+    {
+        if (GetComponentInChildren<Background>() == null)
+        {
+            Debug.LogError("Not found Background component in " + name);
+            Destroy(this);
+        }
+
+        background = GetComponentInChildren<Background>();
+    }
+
+    public void SetSoundState(bool isEnable)
+    {
+        AudioListener.pause = !isEnable;
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        nextSceneLoad = sceneName;
+        background.Fade();
+        background.onCompleteFadeIn = delegate () { SceneManager.LoadScene(sceneName); };
+    }
+
+    public void Quit ()
+    {
+        Application.Quit();
+    }
+}
